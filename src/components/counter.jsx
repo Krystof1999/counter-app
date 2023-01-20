bcada1aabde048d187296f33666fadae79d058a4
DESKTOP-J4A1050\kryst <Krystofbumbalek2@gmail.com>
2023-01-20 17:26:49 +0100

    counter update

---
+++ src/components/counter.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  state = {
+    count: 0,
+    tags: ["tag1", "tag2", "tag3"],
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <span className={this.gedBtnClasses()}>{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
+        <ul>
+          {this.state.tags.map((tag) => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul>
+      </React.Fragment>
+    );
+  }
+
+  gedBtnClasses() {
+    let classes = "btn m-2 btn-";
+    classes += this.state.count === 0 ? "primary" : "secondary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
+}
+
+export default Counter;
